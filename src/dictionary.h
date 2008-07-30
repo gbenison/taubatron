83d9e106617c140c0e23656f9830ec21603ab9b7
greg <greg@gblaptop>
2008-07-29 18:03:11 -0700

    started implementing sample dictionary

--- src/dictionary.h
+++ src/dictionary.h
@@ -1,16 +1,36 @@
+/*
+ *  Copyright (C) 2008 Greg Benison
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
-/* FIXME how about using a hash instead?? */
+#include <glib.h>
+
 struct _word
 {
   gchar *chars;
   guint  id;
 };
 
-typdef struct _word word_t;
-typdef struct _dictionary dictionary_t;
+typedef struct _word word_t;
+typedef struct _dictionary dictionary_t;
 
 typedef void (*DictionaryCallback) (word_t *word, gpointer data);
 
+dictionary_t* dictionary_new    (void);
 word_t* dictionary_append       (dictionary_t* dict, gchar* word);
 word_t* dictionary_lookup_chars (dictionary_t* dict, gchar* word);
 word_t* dictionary_lookup_id    (dictionary_t* dict, guint id);
--- src/rule.h
+++ src/rule.h
@@ -1,5 +1,26 @@
+/*
+ *  Copyright (C) 2008 Greg Benison
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
-/* FIXME */
+#ifndef _HAVE_RULE_H
+#define _HAVE_RULE_H
+
+
 
 /*
 
@@ -29,3 +50,5 @@ Example rules
 2) 'deletion of one letter'
 
 */
+
+#endif /* not  _HAVE_RULE_H */
